6c4b662737141300d12fd2f8ad563be57efbfffc
khongpan <khongpan@gmail.com>
2017-04-06 23:06:40 +0700

    add restore default setting

---
+++ config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+
+#define CONFIG_H
+
+extern char wifi_ssid[20];
+extern char wifi_pass[20];
+extern char node_name[20];
+
+void config_restore_default(void);
+void config_restore_myhome(void);
+void config_init(void);
+esp_err_t config_get_key_str(char *key, char * ret_str, char *default_str);
+esp_err_t config_set_key_str(char *key, char *set_str);
+
+#endif
+
